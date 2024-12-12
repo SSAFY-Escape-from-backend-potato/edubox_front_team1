5f95f9dc1d332c9f6cd21985b7241e854630ad0a
Mukho <mukho@kakao.com>
2024-12-12 20:06:19 +0900

    Feat: 회원가입 관련 커스텀 훅 추가

--- src/hooks/usePasswordVisibility.ts
+++ src/hooks/usePasswordVisibility.ts
@@ -1,16 +1,21 @@
 import { useState } from "react";
 
-const usePasswordVisibility = () => {
+export const usePasswordVisibility = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
+  const togglePasswordConfirmVisibility = () => {
+    setShowPasswordConfirm(!showPasswordConfirm);
+  };
+
   return {
     showPassword,
+    showPasswordConfirm,
     togglePasswordVisibility,
+    togglePasswordConfirmVisibility,
   };
 };
-
-export default usePasswordVisibility;
