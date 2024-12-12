2381ab311c61b5f5972643ca06880a1f8037dbb7
Mukho <mukho@kakao.com>
2024-12-12 16:43:48 +0900

    Refact: SocialLogin 컴포넌트 이동 및 수정

--- src/components/common/SocialLogin.tsx
+++ src/components/common/SocialLogin.tsx
@@ -3,10 +3,10 @@ import GoogleButton from "./Button/GoogleButton";
 import NaverButton from "./Button/NaverButton";
 import "./SocialLogin.css";
 
-const SocialLogin = () => {
+const SocialLogin = ({ text }: { text: string }) => {
   return (
     <div className="social-login">
-      <p>간편 로그인</p>
+      <p>{text}</p>
       <div className="social-btns">
         <KakaoButton />
         <GoogleButton />
